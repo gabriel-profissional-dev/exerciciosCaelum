3da8350ea228e4e90e494c7107f63a8fedf27a6e
Gabriel <gabriel.profissional.dev@gmail.com>
2022-07-06 04:37:04 -0300

    Exercicio 3.1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace tres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Media();
+        }
+
+        static void Media()
+        {
+            double idadeGabriel = 36;
+            double idadeAdryelle = 33;
+            double idadeAdryan = 11;
+
+            double resultado = (idadeGabriel + idadeAdryelle + idadeAdryan)/3;
+
+            Console.Clear();
+            Console.WriteLine(Math.Round(resultado,2));
+            Console.ReadKey();
+        }
+    }
+}
